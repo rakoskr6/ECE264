e0b6027df9b80943062d129305616897e1dbfafe
rakoskr6 <krakos@purdue.edu>
2014-09-08 13:58:57 -0400

    Added my_strlen and my_countcharls

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include "answer02.h"
+size_t my_strlen(const char * str) ;
 
-int main (int argc, char * * argv)
+int main (void)
 {
-	printf("Hello World!\n");
-	// Create your test-case here, good luck!
+char * str = "hello!";
+printf("%i\n",my_countchar(str, 'k'));
+return 0;
+}
+
+
+// Must point to valid string, cannot be a null string
+size_t my_strlen(const char * str) //size_t is unsigned integer
+{
+	int index = 0;
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		// For loop runs until end of string is found.
+		// No need for anything in the loop since the 
+		// the function returns index.
+	}
+	return index;
+}
+
+
+int my_countchar(const char * str, char ch)
+{
+	int index = 0, Occurances = 0;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		if (str[index] == ch)
+		{
+			Occurances++;	
+		}
+	}
+	
+	return Occurances;
 }
