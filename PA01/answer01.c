5f6bfd85eefc031500064e9c0e4415a6e8bb4631
rakoskr6 <krakos@purdue.edu>
2014-09-08 21:40:57 -0400

    Random update

--- PA01/answer01.c
+++ PA01/answer01.c
@@ -6,6 +6,7 @@ int arraySum(int * array, int len)
   int index = 0;
   int sum = 0;
 
+  // Itterates through the whole array adding all numbers
   for (index = 0; index < len; index++)
     {
       sum += array[index];
@@ -18,6 +19,8 @@ int arrayCountNegative(int * array, int len)
 {
   int index = 0;
   int NumNegative = 0;
+
+  // Itterates through the whole array counting all numbers < 0
   for (index = 0; index < len; index++)
     {
       if (array[index] < 0)
@@ -34,7 +37,7 @@ int arrayIsIncreasing(int * array, int len)
   int indexminus = 0; // Numer before the current index
  
 
-  // Itterates over entire array
+  // Itterates over entire array, only finishes loop if arrays increasing
   for (index = 0; index < len; index++)
     {
       indexminus = index - 1;
@@ -45,6 +48,7 @@ int arrayIsIncreasing(int * array, int len)
 	  indexminus = index;
 	}      
 
+      // If the array isn't increasing return 0
       if (array[index] < array [indexminus])
 	{
 	  return 0;
@@ -58,7 +62,7 @@ int arrayIndexRFind(int needle, const int * haystack, int len)
   int index = 0;
   int NeedleIndex = -1;
 
-
+  // Itterates over entire array looking for the needle
   for (index = 0; index < len; index++)
     {
       if (haystack[index] == needle)
@@ -76,13 +80,16 @@ int arrayFindSmallest(int * array, int len)
   int smallest;
   int smallestindex;
 
+  // Ensures there is at least 1 number in the array
   if (len <= 0)
     {
       return 0;
     }
 
-  smallest = array[len-1];
+  smallest = array[len-1]; // Initial value of smallest in case all numbers are the same
 
+  // Itterates over entire array right to left (so rightmost number is used
+  // if multiple lowest numbers exist)
   for (index = len - 1; index >= 0; index--)
     {
       if (array[index] <= smallest)
