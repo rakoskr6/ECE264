c170f669937d917cbc0da1483d7e3cab01621db3
rakoskr6 <krakos@purdue.edu>
2014-09-22 12:39:58 -0400

    Added more comments and cleaned up the format

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -2,28 +2,26 @@
 char * strcat_ex(char * * dest, int * n, const char * src) // Append src to dest
 {
 	int DestLength = 0, SrcLength = 0;
-	//int SrcIndex = 0;
-	//int DestIndex = 0;
 	int Increase = 0;
 	char * dest2;
 
-	
 	DestLength = strlen(*dest);
 	SrcLength = strlen(src);
-	printf("Dest %i, Src %i\n\n",DestLength,SrcLength);
 	
 	if( (*dest == NULL) || (SrcLength + DestLength + 1 > *n) ) // dest is not large enough - malloc
 	{
-		printf("Going to use malloc\n\n");
 		Increase = SrcLength + DestLength + 1;
 		dest2 = malloc(Increase * sizeof(char));
 		strcpy(dest2,*dest);
 		free(*dest);
-		printf("%s\n\n",dest2);
 	    *dest = dest2;
 	    strcpy(*dest + DestLength,src);
-
-}
+	}
+	else
+	{
+	    strcpy(*dest + DestLength,src);
+		
+	}
 		
 	
 	
