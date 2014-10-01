8ecc04b37dfd0be2401025e90696565890906093
rakoskr6 <krakos@purdue.edu>
2014-10-01 16:55:39 -0400

    Wrote echo program. Initial test show it effectivly works

---
+++ PA04/echo-lite.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char * * argv)
+{
+
+int ind = 1; // Skip 0,the program path
+   
+   // Loop through arguments to print
+   
+   for( ; ind < argc; ++ind) 
+   {
+     
+	fprintf(stdout, "%s ", argv[ind]);
+   }
+   fprintf(stdout, "\n"); // Add newline after complete
+	
+	
+	return EXIT_SUCCESS;
+}
