5311533aec1bd073e5fa000505269fc180e14bdb
rakoskr6 <krakos@purdue.edu>
2014-10-03 15:56:00 -0400

    Fixed buffer overflow

--- PA04/cat-lite.c
+++ PA04/cat-lite.c
@@ -10,22 +10,27 @@ int main(int argc, char * * argv)
 
 int ind = 1; // skip 0, which is the program path
 FILE *FilePtr;
-char Charct;
+char Charct = 'a';
 
 // First loop checks to see if help flag is present
 for( ; ind < argc; ++ind) 
 {
-	   // If --help is passed, show help and end
+	// If --help is passed, show help and end
 	if(strcmp(argv[ind], "--help") == 0) // Shows help if true
 	{
-		printf("<help-message>\n"
-				"Usage: cat-lite [--help] [FILE]...\n"
-				"With no FILE, or when FILE is -, read standard input.\n");
+		printf(	"Usage: cat-lite [--help] [FILE]...\n"
+				"With no FILE, or when FILE is -, read standard input.\n\n"			   
+				"Examples:\n"
+					"cat-lite README   Print the file README to standard output.\n"
+					"cat-lite f - g    Print f's contents, then standard input,\n" 
+									 "then g's contents.\n"
+					"cat-lite          Copy standard input to standard output.\n"
+				);
 		return EXIT_SUCCESS;
 	}
 }
 
-// Second loop checks to ensure all files exist (and not think - is a file)
+// Second loop checks to ensure all files exist (and ignore -)
 for(ind = 1; ind < argc; ++ind) 
 {		
 	if (((fopen(argv[ind],"r")) == NULL) && ((strcmp(argv[ind],"-")) != 0 ))
@@ -46,8 +51,8 @@ for(ind = 1; ind < argc; ++ind)
 		{
 			Charct = fgetc(stdin);
 			fprintf(stdout,"%c",Charct);
-			
 		}
+		return EXIT_SUCCESS;
 	}
 
 	else
@@ -58,10 +63,18 @@ for(ind = 1; ind < argc; ++ind)
 			fprintf(stdout,"%c",Charct);
 			Charct = fgetc(FilePtr);
 		}
-		
+		fclose(FilePtr);
 	}
+	return EXIT_SUCCESS;
 		
 }
 
+// Added so leaving arguments blank is same as -
+while (Charct != '\n')
+	{
+		Charct = fgetc(stdin);
+		fprintf(stdout,"%c",Charct);
+	}
+		
 	return EXIT_SUCCESS;
 }
