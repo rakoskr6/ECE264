51a268e41e476b52e70ae1ddc0675ac2760fafc3
rakoskr6 <krakos@purdue.edu>
2014-10-08 14:46:20 -0400

    Changed PartitionIn to call itself instead of Partition (correct recursion)

---
+++ PA05/answer05.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "answer05.h"
+// Function Prototypes
+void Partition (int * arr, int ind, int left);
+void PartitionIn(int * PartArr, int ind, int left);
+
+void partitionAll(int PartitionNum) // Code used (and modified) from class book
+{
+	int * PartArr;
+	PartArr = malloc(sizeof( int ) * PartitionNum);
+	
+	Partition(PartArr, 0 , PartitionNum); // Call function which will actually use recursion
+	
+	free(PartArr);
+}
+
+void Partition(int * PartArr, int ind, int left)
+{
+	int val = 1, PrintInd = 0;
+	/* left is how many integer options are left after the first integer is chosen
+	 * val is the value that is part of the partition
+	 * ind is the order in which the previously found val will go */
+	
+	
+	if (left == 0) // Then print array to show partition
+	{
+		for (PrintInd = 0; PrintInd < ind - 1; PrintInd++)
+		{
+			printf("%d + ",PartArr[PrintInd]);
+		}
+		printf("%d\n",PartArr[ind - 1]); // Out of loop to allow \n
+	}
+	
+	for(val = 1; val <= left; val ++)
+	{
+		PartArr[ind] = val;
+		Partition(PartArr, ind + 1 , left - val);
+	}
+}
+
+
+
+void partitionIncreasing(int PartitionNum)
+{
+	printf("Partition Increasing:\n");
+	int * PartArr;
+	PartArr = malloc(sizeof( int ) * PartitionNum);
+	
+	PartitionIn(PartArr, 0 , PartitionNum); // Call function which will actually use recursion
+	
+	free(PartArr);
+}
+
+void PartitionIn(int * PartArr, int ind, int left)
+{
+	int val = 1, PrintInd = 0;
+	
+	if (left == 0) // Then print array to show partition
+	{
+		for (PrintInd = 0; PrintInd < ind - 1; PrintInd++)
+		{
+			printf("%d + ",PartArr[PrintInd]);
+		}
+		printf("%d\n",PartArr[ind - 1]); // Out of loop to allow \n
+	}
+	
+	for(val = 1; val <= left; val ++)
+	{
+		if ((ind - 1) >= 0) // Makes sure a valid location in array will be derefference
+		{
+			if (val > PartArr[ind - 1]) // Ensure current value > previous value
+			{
+				PartArr[ind] = val;
+				PartitionIn(PartArr, ind + 1, left - val);
+			}
+
+		}
+		else
+		{
+			PartArr[ind] = val;
+			PartitionIn(PartArr, ind + 1 , left - val);
+		}
+	}
+}
