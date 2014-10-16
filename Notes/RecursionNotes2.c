d341a521f6f6994279eaea1b94a14638364bff07
rakoskr6 <krakos@purdue.edu>
2014-10-16 09:36:41 -0400

    Added notes

---
+++ Notes/RecursionNotes2.c
@@ -0,0 +1,9 @@
+Mathematical Induction
+	1. Base Case
+	2. Assume it is true up to n
+	3. Prove it is true to n+1
+
+for n = 4; // Program to print all partitions
+
+PartArr = [x,x,x,x]
+Partition(PartArr, 0, 4)
