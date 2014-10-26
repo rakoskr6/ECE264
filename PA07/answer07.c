6aff58c18e4770f0f9cffb62413c5d559c3c33e5
rakoskr6 <krakos@purdue.edu>
2014-10-25 20:08:19 -0400

    Added answer07.c

---
+++ PA07/answer07.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "answer07.h"
+
+//Image * Image_load(const char * filename)
+//{
+	
+//}
+
+
+
+int Image_save(const char * filename, Image * image)
+{
+
+return EXIT_SUCCESS;	
+}
+
+
+
+void Image_free(Image * image)
+{
+	
+}
+
+
+
+void linearNormalization(int width, int height, uint8_t * intensity)
+{
+	
+}
