b5566d887691ecc540f8f245d86a4312baa223aa
rakoskr6 <krakos@purdue.edu>
2014-11-09 17:49:16 -0500

    Added function to print tree and node

--- PA09/answer09.c
+++ PA09/answer09.c
@@ -16,7 +16,7 @@ BusinessNode * create_node(char * stars, char * name, char * address)
 
 BusinessNode *tree_insert(BusinessNode * node, BusinessNode * root)
 {
-	if(strcmp(root->name,node->name) > //root before node, right side
+	if(strcmp(root->name,node->name) > 0)//root before node, right side
 	{
 		if (root->right == NULL)
 		{
@@ -37,6 +37,32 @@ BusinessNode *tree_insert(BusinessNode * node, BusinessNode * root)
 		{
 			tree_insert(node,root->left);
 		}
+	}
+	return node;
+	
+}
+
+void print_node(BusinessNode * node)
+{
+	printf("Name: %s\nAddress: %s\n%s/5.0 stars\n\n",node->name,node->address,node->stars);
+}
+
+void print_tree2(BusinessNode * tree,int i)
+{
+	if (tree != NULL)
+	{
+		printf("%i\n",i++);
+		print_node(tree);
+	}
+	if (tree->left != NULL)
+	{
+		printf("Left");
+		print_tree2(tree->left,i);
+	}
+	if (tree->right != NULL)
+	{
+		printf("Right");
+		print_tree2(tree->right,i);
 	}
 	
 }
--- PA09/test.c
+++ PA09/test.c
@@ -2,23 +2,22 @@
 
 int main(void)
 {
+	void print_tree2(BusinessNode * tree);
 	printf("\e[1;1H\e[2J"); // clears screen
-	char stars[] = "4.5";
-	char name[] = "Awesome Business";
-	char address[] = "Purdue Road, Purdue";
-	BusinessNode *Node;
+	BusinessNode *Node,*Node1, *Node2, *Node3, *Node4;
 	
-	Node = create_node(stars,name,address);
+	Node = create_node("4.5","Awesome Business","Purdue");
 	Node1 = create_node("3.2","Yummy","here");
 	Node2 = create_node("3.9","K","There");
 	Node3 = create_node("5.0","La sta","DC");
 	Node4 = create_node("0.9","App","Gary");
 	
 	
-	printf("The name is %s, address is %s with %s stars\n",Node->name,Node->address,Node->stars);
-	printf("The name is %s, address is %s with %s stars\n",Node1->name,Node1->address,Node1->stars);
-	printf("The name is %s, address is %s with %s stars\n",Node2->name,Node2->address,Node2->stars);
-	printf("The name is %s, address is %s with %s stars\n",Node3->name,Node3->address,Node3->stars);
-	printf("The name is %s, address is %s with %s stars\n",Node4->name,Node4->address,Node4->stars);
+	print_node(Node);
+	print_node(Node1);
+	print_node(Node2);
+	print_node(Node3);
+	print_node(Node4);
+	
 	return EXIT_SUCCESS;
 }
