ef708aedbed5a3fc81f0bb769fdac4be6bcf6477
rakoskr6 <krakos@purdue.edu>
2014-11-09 17:31:21 -0500

    Added create_node function

--- PA09/answer09.c
+++ PA09/answer09.c
@@ -1,1 +1,42 @@
+#include "answer09.h"
 
+BusinessNode * create_node(char * stars, char * name, char * address)
+{
+	// Allocates size for node
+	BusinessNode *NewNode = malloc(sizeof(BusinessNode));
+	// Doesn't need strdup, uses given pointers
+	NewNode->stars = stars;
+	NewNode->name = name;
+	NewNode->address = address;
+	// Makes next nodes null
+	NewNode->left = NULL;
+	NewNode->right = NULL;
+	return  NewNode;
+}
+
+BusinessNode *tree_insert(BusinessNode * node, BusinessNode * root)
+{
+	if(strcmp(root->name,node->name) > //root before node, right side
+	{
+		if (root->right == NULL)
+		{
+			root->right = node;
+		}
+		else
+		{
+			tree_insert(node,root->right);
+		}
+	}
+	else //root after node, left side
+	{
+		if (root->left == NULL)
+		{
+			root->left = node;
+		}
+		else
+		{
+			tree_insert(node,root->left);
+		}
+	}
+	
+}
