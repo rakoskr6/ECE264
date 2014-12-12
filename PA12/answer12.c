484a5e0f0e80a4fe0a18d7e971f945f442e7d68b
rakoskr6 <krakos@purdue.edu>
2014-12-11 20:55:43 -0500

    Fixecd converting to int error with too large of numbers

--- PA12/answer12.c
+++ PA12/answer12.c
@@ -6,32 +6,80 @@
 #include "answer12.h"
 
 uint128 alphaTou128(const char * str)
-{//Convert the initial portion of C-string 'str' to an unsigned 128-bit integer
-	int index = 0, tempint, place = 0;
-	uint128 FinalNumber = 0;
-	place = strlen(str) - 1; // minus one since place should end at 0
+{
+	int index = 0; // Index to itterate through array
+	int Negative = 0; // 0 if positive, 1 if negative
+	uint128 Number = 0; // The number that is returned
+	long int Place = 1; // The place value of the leftmost digit
 	
-	if (((str[index] > '9') || (str[index] < '0')) && (str[index] != ' '))
-			{
-				return 0; // random characters start string, return 0
-			}
-			
-	for (index = 0; index < strlen(str); index++)
+	while(str[index] != '\0')
 	{
-		if (str[index] != ' ')
-		{
-			if ((str[index] > '9') || (str[index] < '0'))
-			{
-				return FinalNumber; // random characters in string, return final number
-			}
-			tempint = str[index] - '0'; // current value as int
-			FinalNumber += powl(10,place)*tempint; // Add to uint128 with place value factor
-			place--; // decrement place value
-		}
+		index++;
 	}
 	
-	return FinalNumber;
+	
+	for (; index>= 0; index--) // Runs until end of string
+	{
+		if (str [index] == '-') // If a '-' is in the string, the number is negative
+		{
+			Negative = 1;
+		}
+		
+		
+		if (str[index] == '0' || str[index] == '1' || str[index] == '2' || str[index] == '3' || str[index] == '4' || str[index] == '5' || str[index] == '6' || str[index] == '7' || str[index] == '8' || str[index] == '9') 
+		{
+			// Multiply the previous number by place and add the new number to the 1's place.
+			// Also subtract by ASCII value '0' since str[index] - '0' will be the number of str[index].
+			Number = Number + Place * (str[index] - '0');
+			Place = Place * 10; // Increment the place of the number by a factor of 10
+		}
+	}
+	// Makes number negative if the negative flag is 1
+	if (Negative == 1) 
+	{
+		Number = 0;
+	}
+	
+	return Number;
+	
 }
+	
+//uint128 alphaTou128(const char * str)
+//{//Convert the initial portion of C-string 'str' to an unsigned 128-bit integer
+	//int index = 0, tempint, place = 0;
+	//uint128 FinalNumber = 0;
+	
+	//while(str[index] == ' ') // ignores leading whitespace
+	//{
+		//index++;
+	//}
+	
+	//while (((str[index] > '9') || (str[index] < '0')))
+	//{
+		//place++;
+	//}
+	////place = strlen(str) - 1; // minus one since place should end at 0
+	
+	//if (((str[index] > '9') || (str[index] < '0')))
+			//{
+				//return 0; // random characters start string, return 0
+			//}
+			
+	//for (index = 0; index < strlen(str); index++)
+	//{
+		//if ((str[index] > '9') || (str[index] < '0'))
+		//{
+			//return FinalNumber; // random characters in string, return final number
+		//}
+		//tempint = str[index] - '0'; // current value as int
+		//printf("%i %Le   ",tempint,powl(10,place));
+		//FinalNumber += powl(10,place)*tempint; // Add to uint128 with place value factor
+		//printf("%c\n",str[index]);
+		//place--; // decrement place value
+	//}
+	
+	//return FinalNumber;
+//}
 
 char * u128ToString(uint128 value) // ensure string can be added big to lower index, Temp should only 
 {
--- PA12/test.c
+++ PA12/test.c
@@ -9,7 +9,8 @@ int main(void)
 	uint128 value;
 	char *sValue;
 	
-	value = alphaTou128(" 340282366920938463463374607431768211455th");
+	//value = alphaTou128(" 340282366920938463463374607431768211455th");
+	value = alphaTou128(" 12345ab");
 	sValue = u128ToString(value);
 	printf("%s is %i\n",sValue, primalityTestParallel(value, 1));
 	printf("%i is %i\n",5, primalityTestParallel(5, 1));
